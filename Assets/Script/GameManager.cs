4858d9ba494111bd3a3c856dc570e747826f01cc
psj1208 <162458734+psj1208@users.noreply.github.com>
2025-02-21 11:54:43 +0900

    [feat] 박성준_씬 추가 및 인코딩 확인

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // 인코딩 확인용 주석입니다.
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
