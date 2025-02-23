c732ddfe5e6ddf40870f3a9dc8de06f3525960b2
leewootak <leewt32414@gmail.com>
2025-02-23 23:04:42 +0900

    [feat] 이우탁_플레이어 이동 및 점프 구현

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -5,13 +5,79 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private bool isDead;
+    [SerializeField] private bool isJumping;
     private bool isSliding;
-    private bool isJump;
-    private int jumpCount;
+
+    [SerializeField] private float speed;
+    [SerializeField] private int jumpForce;
+    [SerializeField] private int jumpCount = 2;
 
     private StatHandler statHandler;
     private AnimationHandler animationHandler;
 
     private BoxCollider2D col;
     private Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<BoxCollider2D>();
+        animationHandler = GetComponent<AnimationHandler>();
+        statHandler = GetComponent<StatHandler>();
+    }
+
+    void Start()
+    {
+        if (rb == null)
+        {
+            Debug.Log("Not Founded Rigidbody");
+        }
+
+        isDead = false;
+        isJumping = false;
+    }
+
+    void Update()
+    {
+        if (isDead == false)
+        {
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                isJumping = true;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (isDead == true) return;
+
+        Vector3 velocity = rb.velocity;
+        velocity.x = speed;
+        rb.velocity = velocity;
+
+        if (isJumping == true)
+        {
+            Jump();
+        }
+
+        Debug.DrawRay(rb.position, Vector3.down, Color.green);
+        RaycastHit2D rayHit = Physics2D.Raycast(rb.position, Vector3.down, 1f, LayerMask.GetMask("Ground"));
+
+        if (rayHit.collider != null)
+        {
+            isJumping = false;
+            jumpCount = 2;
+        }
+    }
+
+    void Jump()
+    {
+        if (jumpCount >= 0)
+        {
+            rb.velocity = Vector3.up * jumpForce;
+            jumpCount--;
+        }
+    }
 }
