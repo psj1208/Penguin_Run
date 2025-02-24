f283080b67b08555f6352521f96dc65b33c3e39e
subin <subin24890@gmail.com>
2025-02-24 15:11:21 +0900

    [feat] 조수빈_플레이어 카메라 테스트 작성

--- Assets/Scripts/Object/FollowCamera.cs
+++ Assets/Scripts/Object/FollowCamera.cs
@@ -9,8 +9,8 @@ public class FollowCamera : MonoBehaviour
 
     void Start()
     {
-        /*플레이어와 카메라의 간격값
-        카메라의 x축 포지션값 - 카메라의 x축 포지션값 = offsetX
+        /*카메라의 간격값
+        카메라의 x축 포지션값 - 플레이어의 x축 포지션값 = offsetX
         */
         if (target == null) return;
         offsetX = transform.position.x - target.position.x;
@@ -21,11 +21,12 @@ public class FollowCamera : MonoBehaviour
     {
         if (target == null) return;
 
-        Vector3 CameraPos = transform.position;//카메라위치
-        /*위에서 구한 간격값에 카메라위치 더하기
+        Vector3 cameraPos = transform.position;//카메라위치
         
+        /*위에서 구한 간격값에 카메라위치 더하기
+        플레이어 위치에 카메라 위치 x축 값을 더해서  CameraPos 변수에 지정
         */
-        CameraPos.x = transform.position.x + offsetX;
-        transform.position = CameraPos;
+        cameraPos.x = target.position.x + offsetX;
+        transform.position = cameraPos;
     }
 }
