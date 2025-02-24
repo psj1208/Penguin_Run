9b9be3c9f24519b220d29268cda4f3deeead0688
subin <subin24890@gmail.com>
2025-02-24 14:49:02 +0900

    [fix] 조수빈_스크립트수정

--- Assets/Scripts/Object/FollowCamera.cs
+++ Assets/Scripts/Object/FollowCamera.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform target;
+    float offsetX;
+
     void Start()
     {
-        
+        /*플레이어와 카메라의 간격값
+        카메라의 x축 포지션값 - 카메라의 x축 포지션값 = offsetX
+        */
+        if (target == null) return;
+        offsetX = transform.position.x - target.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (target == null) return;
+
+        Vector3 CameraPos = transform.position;//카메라위치
+        /*위에서 구한 간격값에 카메라위치 더하기
         
+        */
+        CameraPos.x = transform.position.x + offsetX;
+        transform.position = CameraPos;
     }
 }
