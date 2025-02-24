c3d7f5fabe23323686f32b7206c3ac9501b745a9
psj1208 <162458734+psj1208@users.noreply.github.com>
2025-02-24 15:57:17 +0900

    [feat] 박성준_아이템 흭득 효과 구현

--- Assets/Scripts/Object/Item.cs
+++ Assets/Scripts/Object/Item.cs
@@ -10,16 +10,25 @@ public class Item : InteractObject
     [SerializeField] private int hpValue = 0;
     [SerializeField] private int speedValue = 0;
     [SerializeField] private int Score = 0;
+    ParticleSystem particle;
+    bool isActive = true;
+
+    private void Start()
+    {
+        particle = GetComponent<ParticleSystem>();
+    }
 
     public override void OnInteraction(PlayerController pController)
     {
-        if (pController == null)
+        if (pController == null || isActive == false)
             return;
+        ParticleAndDestroy();
         switch (itemType)
         {
             case ItemType.Heal:
                 Debug.Log($"{hpValue} 체력 조절!");
                 pController.ChangeHP(hpValue);
+                CoinsManager.Instance.AddHearts(this.transform.position, hpValue, pController);
                 break;
             case ItemType.Speed:
                 Debug.Log($"{speedValue} 속도 조절!");
@@ -28,9 +37,17 @@ public class Item : InteractObject
             case ItemType.Score:
                 //매니저에서 스코어 증가 함수 필요
                 Debug.Log($"{Score} 스코어 증가!");
+                CoinsManager.Instance.AddCoins(this.transform.position, Score, pController);
                 break;
             default:
                 break;
         }
+    }
+    
+    void ParticleAndDestroy()
+    {
+        particle.Play();
+        isActive = false;
+        GetComponentInChildren<SpriteRenderer>().enabled = false;
     }
 }
