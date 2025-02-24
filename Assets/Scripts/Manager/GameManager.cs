5dd9bb600d360d9217f40792df432995701b19bd
subin <subin24890@gmail.com>
2025-02-24 10:29:26 +0900

    [feat] 조수빈_프로퍼티 및 ui 호출, 타임스케일 설정

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,3 +1,4 @@
+using DataDeclaration;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -9,29 +10,56 @@ public class GameManager : MonoBehaviour
 
     private PlayerController player;
 
-    UIManager uiManager;
+    private UIManager uiManager;
 
-    //테스트용 난이도 설정
-    float testlevel;
+    //test용 필요없을시 삭제 예정
+    float hpTime = 180f;
+    int levelSpeed = 1;
+    int bestScore;
+    int resultScore;
+    int CurrentScore;
 
+    public GameManager gameManager { get { return gameManager; } }
 
-    private void Update()
+    private void Awake()
     {
-
+        //싱글톤
+        if (instance == null)
+        {
+            instance = this;
+        }
+        //컴포넌트 정보 변수에 지정
+        player = gameObject.GetComponent<PlayerController>();
+        uiManager = gameObject.GetComponent<UIManager>();
     }
+    /// <summary>
+    /// 게임시작 시 메뉴 호출
+    /// </summary>
+    void Start()
+    {
+        Time.timeScale = 0f;
 
+        StartGame();
+    }
+    /// <summary>
+    /// 게임 시작 메뉴 호출
+    /// </summary>
     public void StartGame()
     {
-        //uiManager.SetPlayGame(); ui매니져 작업 완료시 연결
+        Time.timeScale = 1f;
+        uiManager.ChangeUIState(UIState.Start);//필요한 이넘 값을 
     }
-
+    //아직 예정 된거 없는 메서드
     public void IncreaseLevel()
     {
-        //장애물 빈도 및 속도 증가 (아직 결정된게없음)
-    }
 
+    }
+    /// <summary>
+    /// 게임 오버 메뉴 호출
+    /// </summary>
     public void GameOver()
     {
-        //uiManager.SetGameOver(); ui매니져 작업 완료시 연결
+        Time.timeScale = 0f;
+        uiManager.ChangeUIState(UIState.GameOver);
     }
 }
