9d612792aa4cdc81ffa0bad2d4879b8957d2f9b4
Jaewon <dhwodnjs0827@naver.com>
2025-02-25 21:09:27 +0900

    [chore] 오재원_씬 연결
    - StartScene에서 StageScene으로 연결

--- Assets/Scripts/UI/StartSceneUI.cs
+++ Assets/Scripts/UI/StartSceneUI.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartSceneUI : MonoBehaviour
 {
     public void StartSceneBtn()
     {
-        Debug.Log("Test 구동");
+        SceneManager.LoadScene(1);
     }
 }
