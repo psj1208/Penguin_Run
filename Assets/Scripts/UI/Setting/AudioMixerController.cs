43563d5940cb09eba25680c0f2e563a5e6bc8011
subin <subin24890@gmail.com>
2025-02-26 19:53:13 +0900

    [fix] 조수빈_사운드믹스 컨트롤러 수정

--- Assets/Scripts/UI/Setting/AudioMixerController.cs
+++ Assets/Scripts/UI/Setting/AudioMixerController.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioMixerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider masterSlider;
+    [SerializeField] private Slider backGroundSlider;
+    [SerializeField] private Slider sfxSlider;
+
+
+    private void Awake()
     {
-        
+        masterSlider.onValueChanged.AddListener(MasterVolume);
+        backGroundSlider.onValueChanged.AddListener(BackGroundVolume);
+        sfxSlider.onValueChanged.AddListener(SfxVolume);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MasterVolume()
     {
-        
+        audioMixer.SetFloat("Master", Mathf.Log10(volume) * 20);
+    }
+
+    public void BackGroundVolume()
+    {
+        audioMixer.SetFloat("Master", Mathf.Log10(volume) * 20);
+    }
+
+    public void SfxVolume()
+    {
+        audioMixer.SetFloat("Master", Mathf.Log10(volume) * 20);
     }
 }
