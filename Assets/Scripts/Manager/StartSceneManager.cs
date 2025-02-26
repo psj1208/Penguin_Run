b8dcb194dfe252e0deb9ab73fd71afa4c9c1f7a8
Jaewon <dhwodnjs0827@naver.com>
2025-02-26 17:56:05 +0900

    [feat] Fade 기능 추가
    # StartSceneManager.cs
    - Fade용 Image 변수 제거 및 CanvasGroup 변수 추가

--- Assets/Scripts/Manager/StartSceneManager.cs
+++ Assets/Scripts/Manager/StartSceneManager.cs
@@ -6,41 +6,24 @@ using UnityEngine.UI;
 
 public class StartSceneManager : MonoBehaviour
 {
-    private float elapsedTime;
     private float fadeTime;
     private AudioClip btnSFX;
 
     [SerializeField] private Button startBtn;
-    [SerializeField] private Image fadeInImage;
+    [SerializeField] private CanvasGroup fader;
 
     private void Awake()
     {
-        elapsedTime = 0f;
         fadeTime = 1f;
         btnSFX = Resources.Load<AudioClip>("Sounds/Coin/coin01");
 
         startBtn.onClick.AddListener(OnClickStartButton);
         startBtn.onClick.AddListener(() => { AudioManager.PlayClip(btnSFX); });
-        fadeInImage.color = Color.clear;
+        fader.alpha = 0f;
     }
 
     private void OnClickStartButton()
     {
-        StartCoroutine(FadeIn());
-    }
-
-    private IEnumerator FadeIn()
-    {
-        while (elapsedTime < fadeTime)
-        {
-            elapsedTime += Time.deltaTime;
-            float alpha = elapsedTime / fadeTime;
-
-            fadeInImage.color = Color.Lerp(Color.clear, Color.black, elapsedTime / fadeTime);
-
-            yield return null;
-        }
-        fadeInImage.color = Color.black;
-        SceneManager.LoadScene(1);
+        StartCoroutine(FadeHelper.Fade(fader, 0f, 1f, fadeTime, () => SceneManager.LoadScene(1)));
     }
 }
