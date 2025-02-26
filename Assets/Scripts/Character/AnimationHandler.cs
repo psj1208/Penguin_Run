5b260c3963035cbc22bc3301dc1ca6b8330933c4
leewootak <leewt32414@gmail.com>
2025-02-26 21:21:42 +0900

    [comment] 애니메이션 핸들러 주석 작성

--- Assets/Scripts/Character/AnimationHandler.cs
+++ Assets/Scripts/Character/AnimationHandler.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class AnimationHandler : MonoBehaviour
 {
-    private static readonly int IsMoving = Animator.StringToHash("IsMoving"); // 문자를 특정한 숫자 값으로 변환하여 비교
+    // 애니메이터 파라미터 이름을 해시값으로 변환 (문자열 비교 비용 절감)
+    private static readonly int IsMoving = Animator.StringToHash("IsMoving");
     private static readonly int IsDamage = Animator.StringToHash("IsDamage");
     private static readonly int IsSliding = Animator.StringToHash("IsSliding");
     private static readonly int IsJumping = Animator.StringToHash("IsJumping");
@@ -17,11 +18,18 @@ public class AnimationHandler : MonoBehaviour
         animator = GetComponentInChildren<Animator>();
     }
 
+    // 이동 애니메이션
     public void Move()
     {
         animator.SetBool(IsMoving, true);
     }
 
+    /// <summary>
+    /// 점프 애니메이션
+    /// true면 점프 애니메이션을 활성화하고 이동 애니메이션을 비활성화
+    /// false면 점프 애니메이션을 비활성화하고 이동 애니메이션을 활성화
+    /// </summary>
+    /// <param name="ju">점프 여부</param>
     public void SetJump(bool ju)
     {
         if (ju)
@@ -29,40 +37,36 @@ public class AnimationHandler : MonoBehaviour
             animator.SetBool(IsJumping, true);
             animator.SetBool(IsMoving, false);
         }
-        else if (!ju)
+        else
         {
             animator.SetBool(IsJumping, false);
             animator.SetBool(IsMoving, true);
         }
     }
 
+    /// <summary>
+    /// 슬라이딩 애니메이션
+    /// true이면 슬라이딩 애니메이션을 활성화하고 false이면 비활성화
+    /// </summary>
+    /// <param name="sl">슬라이딩 여부</param>
     public void Slide(bool sl)
     {
-        if (sl)
-        {
-            animator.SetBool(IsSliding, true);
-        }
-        else if (!sl)
-        {
-            animator.SetBool(IsSliding, false);
-        }
+        animator.SetBool(IsSliding, sl);
     }
 
+    // 데미지 애니메이션
     public void Damage()
     {
         animator.SetTrigger(IsDamage);
     }
 
+    /// <summary>
+    /// 무적 상태 애니메이션
+    /// true면 무적 애니메이션 활성화, false면 일반 상태
+    /// </summary>
+    /// <param name="inv">무적 상태 여부</param>
     public void Invincibility(bool inv)
     {
-        if(inv)
-        {
-            animator.SetBool(IsInvincibility, true);
-        }
-        else if(!inv)
-        {
-            animator.SetBool(IsInvincibility, false);
-        }
-        
+        animator.SetBool(IsInvincibility, inv);
     }
 }
