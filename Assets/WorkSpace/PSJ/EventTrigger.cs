60c31d98a5cb62835de9c5c6287e2dffdad78790
psj1208 <162458734+psj1208@users.noreply.github.com>
2025-02-26 11:02:05 +0900

    [feat] 박성준_튜토리얼 중간 완성

--- Assets/WorkSpace/PSJ/EventTrigger.cs
+++ Assets/WorkSpace/PSJ/EventTrigger.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PSJTest : MonoBehaviour
+public class EventTrigger : MonoBehaviour
 {
+    [SerializeField] private int eventId;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag.Equals("Player"))
-        {
-            Debug.Log("이벤트 진입");
-            TutoEventManager.Instance.actionList[1].Invoke();
-        }
+            TutoEventManager.Instance.actionList[eventId].Invoke();
     }
 }
--- Assets/WorkSpace/PSJ/TutoEventManager.cs
+++ Assets/WorkSpace/PSJ/TutoEventManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,7 @@ public class TutoEventManager : MonoBehaviour
 {
     public static TutoEventManager Instance;
     TutoUiManager uiManager;
+    public Dictionary<int, Action> actionList = new Dictionary<int, Action>();
 
     private void Awake()
     {
@@ -14,10 +16,58 @@ public class TutoEventManager : MonoBehaviour
     private void Start()
     {
         uiManager = GetComponent<TutoUiManager>();
+        GenerateDictionary();
     }
-    public void EventOne()
+
+    void GenerateDictionary()
     {
-        uiManager.TextHappen("안녕하세요.", txtPos.Down);
-        uiManager.TextHappen("안녕하세요!!!", txtPos.Up);
+        actionList.Add(1, () =>
+        {
+            uiManager.TextHappen("이 게임은 어떻게 진행하나요?.", txtPos.Down);
+            uiManager.TextHappen("먼저 앞에 있는 장애물을 뛰어넘어봅시다.\n(점프 키 입력)", txtPos.Up, KeyCode.Space, GameManager.Instance.Player.JumpMethod);
+        });
+
+        actionList.Add(2, () =>
+        {
+            uiManager.TextHappen("잘 하셨습니다", txtPos.Up);
+        });
+
+        actionList.Add(3, () =>
+        {
+            uiManager.TextHappen("이번 장애물은 크기가 크네요", txtPos.Down);
+            uiManager.TextHappen("이단 점프를 해봅시다.\n(점프 키 입력)", txtPos.Up, KeyCode.Space, GameManager.Instance.Player.JumpMethod);
+        });
+
+        actionList.Add(4, () =>
+        {
+            uiManager.TextHappen("한 번 더 점프하세요.\n(점프 키 입력)", txtPos.Up, KeyCode.Space, GameManager.Instance.Player.JumpMethod);
+        });
+
+        actionList.Add(5, () =>
+        {
+            uiManager.TextHappen("점프하면 안 될 것 같은데요.", txtPos.Down);
+            uiManager.TextHappen("슬라이딩을 해서 넘어가봅시다.\n(슬라이딩 키 입력)", txtPos.Up, KeyCode.LeftShift);
+        });
+
+        actionList.Add(6, () =>
+        {
+            uiManager.TextHappen("이건 뭐죠?.", txtPos.Down);
+            uiManager.TextHappen("이건 체력 회복 아이템입니다.", txtPos.Up);
+            uiManager.TextHappen("얻으면 좌측 상단의 체력이 회복됩니다.\n체력은 계속 줄어드니 주의하세요.", txtPos.Up);
+        });
+
+        actionList.Add(7, () =>
+        {
+            uiManager.TextHappen("이건 뭐죠?.", txtPos.Down);
+            uiManager.TextHappen("이건 점수 흭득 아이템입니다.", txtPos.Up);
+            uiManager.TextHappen("얻으면 좌측 상단의 점수가 증가합니다.", txtPos.Up);
+        });
+
+        actionList.Add(8, () =>
+        {
+            uiManager.TextHappen("이건 뭐죠?.", txtPos.Down);
+            uiManager.TextHappen("이건 속도 증가 아이템입니다.", txtPos.Up);
+            uiManager.TextHappen("얻으면 일정 시간 무적이 되고 속도가 빨라집니다.", txtPos.Up);
+        });
     }
 }
