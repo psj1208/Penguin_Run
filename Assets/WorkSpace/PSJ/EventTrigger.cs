068b39d6eb0425c21d196466cda9b295849d2155
psj1208 <162458734+psj1208@users.noreply.github.com>
2025-02-26 12:07:14 +0900

    [feat] 박성준_튜토리얼 방식 수정

--- Assets/WorkSpace/PSJ/EventTrigger.cs
+++ Assets/WorkSpace/PSJ/EventTrigger.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class EventTrigger : MonoBehaviour
 {
     [SerializeField] private int eventId;
+    bool eventEnable = true;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag.Equals("Player"))
+        if (collision.tag.Equals("Player") && eventEnable == true)
+        {
             TutoEventManager.Instance.actionList[eventId].Invoke();
+            eventEnable = false;
+        }
     }
 }
